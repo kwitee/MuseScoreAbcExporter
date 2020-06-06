6573a4277c057797c944a3c6fe16132fb5c40374
kwitee <kwitee@gmail.com>
2020-06-06 08:56:04 +0200

    If main added.

--- muse_score_abc_exporter.py
+++ muse_score_abc_exporter.py
@@ -2,16 +2,17 @@ import os
 import subprocess
 import sys
 
-muse_score_path = sys.argv[1]
-muse_score_file_path = sys.argv[2]
-music_xml_file_path = F"{os.path.splitext(muse_score_file_path)[0]}.musicxml"
-output_dir = os.path.dirname(os.path.abspath(muse_score_file_path))
+if __name__ == "__main__":
+    muse_score_path = sys.argv[1]
+    muse_score_file_path = sys.argv[2]
+    music_xml_file_path = F"{os.path.splitext(muse_score_file_path)[0]}.musicxml"
+    output_dir = os.path.dirname(os.path.abspath(muse_score_file_path))
 
-print("Exporting muse score file to musicxml format...")
-subprocess.call([muse_score_path, muse_score_file_path, "--export-to", music_xml_file_path])
+    print("Exporting muse score file to musicxml format...")
+    subprocess.call([muse_score_path, muse_score_file_path, "--export-to", music_xml_file_path])
 
-print("Converting musicxml format to abc...")
-subprocess.call([sys.executable, "xml2abc\\xml2abc.py", music_xml_file_path, "-o", output_dir])
+    print("Converting musicxml format to abc...")
+    subprocess.call([sys.executable, "xml2abc\\xml2abc.py", music_xml_file_path, "-o", output_dir])
 
-print("Removing musicxml file...")
-os.remove(music_xml_file_path)
+    print("Removing musicxml file...")
+    os.remove(music_xml_file_path)
